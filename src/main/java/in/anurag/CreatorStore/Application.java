dd1808bd801a796c0d0a48e73e0bfec40167c182
Anurag-xo <amnrag889@gmail.com>
2026-07-26 13:24:42 +0000

    chore: add dotenv java

--- src/main/java/in/anurag/CreatorStore/Application.java
+++ src/main/java/in/anurag/CreatorStore/Application.java
@@ -1,13 +1,18 @@
 package in.anurag.CreatorStore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  public static void main(String[] args) {
+    // Configuration of dotenv-java
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
+    dotenv.entries().forEach((entry) -> System.setProperty(entry.getKey(), entry.getValue()));
+
+    SpringApplication.run(Application.class, args);
+  }
 }
