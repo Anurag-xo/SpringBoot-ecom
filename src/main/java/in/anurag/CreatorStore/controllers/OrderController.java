040cd2e662f0f7b44539733122bd9924c8a605d4
Anurag-xo <amnrag889@gmail.com>
2026-08-30 11:06:20 +0000

    chore: fix typo in order

--- src/main/java/in/anurag/CreatorStore/controllers/OrderController.java
+++ src/main/java/in/anurag/CreatorStore/controllers/OrderController.java
@@ -1,18 +1,16 @@
 package in.anurag.CreatorStore.controllers;
 
+import in.anurag.CreatorStore.dto.OrderRequest;
+import in.anurag.CreatorStore.entities.Order;
+import in.anurag.CreatorStore.services.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import in.anurag.CreatorStore.services.OrderService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-
-/**
- * OrderController
- */
-
+/** OrderController */
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
@@ -23,7 +21,5 @@ public class OrderController {
   @PostMapping
   public Order createOrder(@Valid @RequestBody OrderRequest OrderRequest) {
     return orderService.createOrder(OrderRequest);
-
   }
-  
 }
