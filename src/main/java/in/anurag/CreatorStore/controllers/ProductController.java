2df78a003a5cd20af7dda5381e2c8872309a96ae
Anurag-xo <amnrag889@gmail.com>
2026-08-30 10:28:22 +0000

    chore: remove the auto generated constructor

--- src/main/java/in/anurag/CreatorStore/controllers/ProductController.java
+++ src/main/java/in/anurag/CreatorStore/controllers/ProductController.java
@@ -14,10 +14,10 @@ public class ProductController {
 
   private final ProductService productService;
 
-  public ProductController(ProductService productService) {
-    this.productService = productService;
-  }
-
+  // public ProductController(ProductService productService) {
+  //   this.productService = productService;
+  // }
+  //
   @PostMapping
   public Product createProduct(@Valid @RequestBody Product product) {
     return productService.createProduct(product);
