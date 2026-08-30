88eb6414b882fc48c091ffaa3ef427d5ecc24f62
Anurag-xo <amnrag889@gmail.com>
2026-08-30 12:21:24 +0000

    update: repo code

--- src/main/java/in/anurag/CreatorStore/repositories/OrderitemRepository.java
+++ src/main/java/in/anurag/CreatorStore/repositories/OrderitemRepository.java
@@ -2,6 +2,8 @@ package in.anurag.CreatorStore.repositories;
 
 import in.anurag.CreatorStore.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /** OrderitemRepository */
+@Repository
 public interface OrderitemRepository extends JpaRepository<OrderItem, Long> {}
