9bf6164d1470fa6e63aa77974c50e3bb737b4e52
Anurag-xo <amnrag889@gmail.com>
2026-09-01 17:03:56 +0000

    feature (Exception): add resource not found exception

---
+++ src/main/java/in/anurag/CreatorStore/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package in.anurag.CreatorStore.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
+}
