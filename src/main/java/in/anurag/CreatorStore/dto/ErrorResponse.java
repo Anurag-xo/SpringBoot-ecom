95c6517958abbc2434ab73313ef9d24041eb6f90
Anurag-xo <amnrag889@gmail.com>
2026-09-01 17:05:26 +0000

    feature(error handling): response

---
+++ src/main/java/in/anurag/CreatorStore/dto/ErrorResponse.java
@@ -0,0 +1,18 @@
+package in.anurag.CreatorStore.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+  private int statusCode;
+  private String message;
+  private String details;
+  private LocalDateTime timestamp;
+}
