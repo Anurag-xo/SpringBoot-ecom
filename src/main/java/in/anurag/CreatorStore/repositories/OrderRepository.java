c2e75de12877ebb2c839aa56d860a5d315dad075
Anurag-xo <amnrag889@gmail.com>
2026-09-05 14:17:20 +0000

    update(order repo): feature/search (add methods to query orders by user)

--- src/main/java/in/anurag/CreatorStore/repositories/OrderRepository.java
+++ src/main/java/in/anurag/CreatorStore/repositories/OrderRepository.java
@@ -1,8 +1,15 @@
 package in.anurag.CreatorStore.repositories;
 
 import in.anurag.CreatorStore.entities.Order;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+public interface OrderRepository extends JpaRepository<Order, Long> {
+  // get all orders for a specific user
+  List<Order> findByUserOrderByCreatedAtDesc(User user);
+
+  // get all orders (for admin view)
+  List<Order> findAllByOrderByCreatedAtDesc();
+}
